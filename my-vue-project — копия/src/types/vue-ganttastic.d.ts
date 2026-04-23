465f89c28b454d22ef0b6982d159ceb3e479d960
Shvigmavigma <luckinmatvej2@gmail.com>
2026-04-23 17:19:57 +0300

    fake merge, remake

--- my-vue-project — копия/src/types/vue-ganttastic.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'vue-ganttastic' {
-  import { DefineComponent } from 'vue';
-  export const GanttChart: DefineComponent<{
-    tasks: any[];
-    config?: any;
-    onTaskUpdate?: (task: any) => void;
-  }>;
-}
